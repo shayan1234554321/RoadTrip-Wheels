2af9507ef699cab0f34a203c829c6900ecb5fd36
shayan1234554321 <kshayan091@gmail.com>
2023-07-14 20:16:30 +0500

    added common api endpoints

--- utilities/common.js
+++ utilities/common.js
@@ -5,3 +5,18 @@ export const colors = {
     black: "#000000",
     blue: "#15C7FF"
 }
+
+export const api = {
+    getCars : "/api/v1/cars" , // method: get
+    
+    getCar : (id) => `/api/v1/car/${id}`, // method: get
+    createCar : "/api/v1/cars", // method: post
+    deleteCar : (id) => `/api/v1/cars/${id}`, // method: delete
+
+    getUser : (username) => `/api/v1/users/${username}`, // method: get
+    createUser : "/api/v1/users", // method: post
+
+    getReservations : (userId) => `/api/v1/users/${userId}/reservations`, // method: get
+    createReservation : (userId) => `/api/v1/users/${userId}/reservations`, // method: post
+    deleteReservation : (userId , id) => `/api/v1/users/${userId}/reservations/${id}` // method: delete
+}
