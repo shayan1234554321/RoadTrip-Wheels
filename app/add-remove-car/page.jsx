26b72363cb4260b843c92e2e3bf85213a5142972
shayan1234554321 <kshayan091@gmail.com>
2023-07-19 02:36:25 +0500

    added responsiveness

--- app/add-remove-car/page.jsx
+++ app/add-remove-car/page.jsx
@@ -30,8 +30,14 @@ const CarItem = ({ name, description, image, cost_per_day }) => {
 };
 
 const AddRemoveCar = () => {
+  const [show, setShow] = useState(false);
 
-  const [ show , setShow ] = useState(false)
+  const [form, setForm] = useState({
+    name: "",
+    cost_per_day: 0,
+    imageFile: null,
+    description: "",
+  });
 
   const response = [
     {
@@ -66,11 +72,16 @@ const AddRemoveCar = () => {
     },
   ];
 
+  const handleChange = (e, name) => {
+    setForm((prev) => ({ ...prev, [name]: e.target.value }));
+  };
 
   return (
     <div className={style.addRemoveContainer}>
       <div className={style.addCar}>
-        <RoundedButton onClick={()=>setShow(true)} color={colors.blue}>ADD CAR</RoundedButton>
+        <RoundedButton onClick={() => setShow(true)} color={colors.blue}>
+          ADD CAR
+        </RoundedButton>
       </div>
       <h1>ALL CARS</h1>
       <div className={style.itemsContainer}>
@@ -78,8 +89,74 @@ const AddRemoveCar = () => {
           return <CarItem key={item.name + i} {...item} />;
         })}
       </div>
-      <Popup show={show} setShow={setShow} >
-        hello
+      <Popup show={show} setShow={setShow}>
+        <form className={style.form}>
+          <div className={style.flexRow}>
+            <h4>Name</h4>
+            <input
+              value={form.name}
+              onChange={(e) => handleChange(e, "name")}
+              type="text"
+              placeholder="Range rover"
+              minLength={3}
+              maxLength={50}
+              required
+            />
+          </div>
+          <div className={style.flexRow}>
+            <h4>Cost / day</h4>
+            <input
+              value={form.cost_per_day}
+              name="cost_per_day"
+              onChange={(e) => handleChange(e, "cost_per_day")}
+              type="number"
+              placeholder="12"
+              min={1}
+              max={1000}
+              required
+            />
+            <h4>$</h4>
+          </div>
+          <div className={`${style.flexRow} ${style.imageInput}`}>
+            <div className={style.flexRow}>
+              <h4>Image</h4>
+              <label htmlFor="getImage">Select Image</label>
+              <input
+                required
+                type="file"
+                accept="image/*"
+                id="getImage"
+                onChange={(e) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    imageFile: e.target.files[0]
+                      ? URL.createObjectURL(e.target.files[0])
+                      : "",
+                  }))
+                }
+              />
+            </div>
+            <img src={form.imageFile} alt="" />
+          </div>
+          <div className={style.flexRow}>
+            <h4>Description</h4>
+            <textarea
+              value={form.description}
+              name="description"
+              onChange={(e) => handleChange(e, "description")}
+              type="text"
+              placeholder="The car for your daily needs"
+              maxLength={250}
+              minLength={5}
+              required
+            />
+          </div>
+          <div className={style.buttonContainer}>
+            <RoundedButton type="submit" color={colors.green}>
+              ADD CAR
+            </RoundedButton>
+          </div>
+        </form>
       </Popup>
     </div>
   );
